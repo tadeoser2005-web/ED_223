d9ea6ea55e9de2783e0a0ad386556954c6a2cf84
tadeoser2005-web <tadeoser2005@gmail.com>
2026-09-18 19:55:55 -0600

    manipulacion de arreglos

--- Arreglos.py
+++ Arreglos.py
@@ -2,3 +2,43 @@ numeros = [10,20,30,40,50]
 print(numeros)
 
 print(numeros[2])
+
+numeros[3] =15
+
+print(numeros)
+
+numeros.append(70)
+
+print(numeros)
+
+numeros.remove(10)
+
+
+print(numeros)
+
+numeros.pop(1)
+
+print(numeros)
+
+
+frutas = ["manzana","pera","uva","mango","maracuya","pina"]
+
+frutas.remove("uva")
+
+print(frutas)
+
+frutas.pop(3)
+
+frutas.append("kiwi")
+
+frutas[2] = "fresa"
+
+print(frutas)
+
+n = int(input("ingresa el tamno del arreglo: "))
+ 
+arreglo = []
+
+m = input("ingresa el numero:")
+arreglo.append(m)
+print(arreglo)
